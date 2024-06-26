2b23b9460ed33a3dc11d9423aaf33851dc997b6c
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-26 23:59:16 +0530

    Counter 2

---
+++ FOR_FRONTEND/JAVASCRIPT/CounterTwo.js
@@ -0,0 +1,20 @@
+var createCounter = function(init) {
+    let temp = init;
+    function increment(){
+        init++;
+        return init;
+    }
+    function decrement(){
+        init--;
+        return init;
+    }
+     function reset(){
+        init = temp;
+        return init;
+    }
+    return {
+        increment : increment,
+        decrement : decrement,
+        reset : reset,
+    };
+};
