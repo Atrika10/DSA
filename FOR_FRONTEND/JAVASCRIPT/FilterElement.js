4e8383c316f0e88c126b7146d5c17bc3b3524014
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-28 23:48:00 +0530

    Filter Elements from Array LC : 2634

---
+++ FOR_FRONTEND/JAVASCRIPT/FilterElement.js
@@ -0,0 +1,10 @@
+var filter = function(arr, fn) {
+    n = arr.length;
+    var filteredArr = new Array();
+    for(let i=0;i<n;i++){
+        if(fn(arr[i],i)){
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+};
