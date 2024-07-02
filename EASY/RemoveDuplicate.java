6238278a88646400ac3d205d815b5b508b86e439
ATRIKA SHOW <readeratrika08@gmail.com>
2024-07-02 23:34:22 +0530

    Remove Duplicate from sorted array way-2 LC : 26

--- EASY/RemoveDuplicate.java
+++ EASY/RemoveDuplicate.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class RemoveDuplicate {
 
     // Time Complexity of this code = O(n) + O(n) => O(2n) => O(n)
@@ -23,8 +25,25 @@ public class RemoveDuplicate {
       
         return k;
     }
+
+    public static int removeDuplicates2(int[] nums) {
+        ArrayList<Integer> al = new ArrayList<>();
+        // add first element
+        al.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if(al.get(al.size()-1) != nums[i]) {
+                al.add(nums[i]);
+            }
+        }
+        // put all the elements in the array from arraylist
+        for(int i=0; i<al.size(); i++) {
+            nums[i] = al.get(i);
+        }
+        return al.size();
+
+    }
     public static void main(String[] args) {
         int num[] = {1,1,2};
-        System.out.println(removeDuplicates(num));
+        System.out.println(removeDuplicates2(num));
     }
 }
